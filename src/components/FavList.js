b05fd8b8f5af36b39215c08a59fa0c4f6cb7d8dc
DylanToporek <dylantoporek@gmail.com>
2021-11-22 12:21:04 -0500

    favorite component styling moved to css

--- src/components/FavList.js
+++ src/components/FavList.js
@@ -4,83 +4,32 @@ import React, {useState, useEffect} from "react";
 function FavList({favorites, onDeleteItem}) {
 
     const favDisplay = () => favorites.map((fav) => {
-        return <div id='fav-container' style={{
-            backgroundColor: 'white',
-            borderStyle: 'solid',
-            borderColor: '#ffcb05',
-            borderWidth: 6,
-            borderRadius: 5,
-            width: 150,
-            height: 215,
-            margin: 5,
-            }} key={fav.id}>
+        return <div id='fav-container' key={fav.id}>
 
-            <p style={{
-                fontFamily: "sans-serif",
-                fontSize: 12
-            }}>{fav.name}</p>
+            <p id='fav-name'>
+                {fav.name}
+            </p>
 
-            <img style={{
-                position: 'relative',
-                maxWidth: '120%',
-                maxHeight: '120%',
-                top: 10,
-                zIndex: 99
-            }} src={fav.img} />
+            <img id='poke-fav-img' src={fav.img} />
 
-            <div style={{
-                position: 'relative',
-                backgroundColor: '#3d7dca',
-                borderRadius: "100%",
-                width: 100,
-                height: 40,
-                zIndex: 88,
-                left: 25,
-                top: -40,
-            }}></div>
+            <div id='img-base'></div>
 
-            <button style={{
-                position: 'relative',
-                top: 0
-            }} onClick={() => onDeleteItem(fav)}>Delete</button>
+            <button id='delete-fav' onClick={() => onDeleteItem(fav)}>Delete</button>
         </div>
     })
 
     return (
-        <div style={{
-            position: 'relative',
-            left: 120,
-            top: 10,
-            width: '95%',
-            height: '100%',
-            textAlign: 'center',
-            }}>
-           <div style={{
-               position: 'relative',
-               borderStyle: "solid",
-               width: 150,
-               height: 50,
-               left: 522,
-               fontFamily: "avenirnext-heavy",
-               fontSize: 25,
-               borderRadius: 5
-                }}>
-                    <p style={{
-                        position: 'relative',
-                        top: -16
-                    }}>Favorites</p>
-                </div>
-                <div style={{
-                    position: "relative",
-                    top: 20,
-                    display: "flex",
-                    flexWrap: "wrap",
-                    height: 600,
-                    width: "100%",
-                    overflow: "auto",
-                    }}>
-                    {favDisplay()}
-                </div>
+        <div id='favorites-page-title-container'>
+           
+           <div id='fav-title-display'>
+                <p id='fav-title'>
+                    Favorites
+                </p>
+            </div>
+                
+            <div id='fav-poke-display'>
+                {favDisplay()}
+            </div>
            
         </div>
     )
